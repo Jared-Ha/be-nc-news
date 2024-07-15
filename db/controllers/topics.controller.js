34471e41e70fd33b7e5c04b02d384b16e6dcba4f
Jared-Ha <jared.harrison7@gmail.com>
2024-07-15 11:42:33 +0100

    remove consoloe log in topics controller

--- db/controllers/topics.controller.js
+++ db/controllers/topics.controller.js
@@ -1,7 +1,6 @@
 const { fetchAllTopics } = require("../models/topics.model");
 
 exports.getAllTopics = (req, res, next) => {
-	console.log("hello controller");
 	fetchAllTopics().then((topics) => {
 		res.status(200).send({ topics });
 	});
